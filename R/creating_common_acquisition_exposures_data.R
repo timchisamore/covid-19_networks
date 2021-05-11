b93f917b68dc9129838959b7325e51625dae7dc9
Tim Chisamore <tim.chisamore@gmail.com>
2021-05-11 16:27:58 -0400

    Adding a target to create common acquisition exposure data.

---
+++ R/creating_common_acquisition_exposures_data.R
@@ -0,0 +1,37 @@
+#' Creating common acquisition exposures data
+#'
+#' This function takes the clean CCM exposure investigation - acquisition,
+#' exposures, and investigations data and returns the exposures and locations
+#' with more than one case attached in the past 14 days.
+#'
+#' @param clean_ccm_exposure_investigations_acquisition_data A tbl_df of clean
+#' CCM exposure investigations - acquisition data.
+#' @param clean_ccm_exposures_data A tbl_df of clean CCM exposures data.
+#' @param clean_ccm_investigations_data A tbl_df of clean CCM investigations
+#' data.
+#'
+#' @return A tbl_df of common acquisition exposures data.
+#' @export
+#'
+#' @examples
+#' `creating_common_acquisiton_exposures_data(clean_ccm_exposure_investigations_acquisition_data, clean_ccm_exposures_data, clean_ccm_investigations_data)`
+creating_common_acquisition_exposures_data <- function(clean_ccm_exposure_investigations_acquisition_data, clean_ccm_exposures_data, clean_ccm_investigations_data) {
+  clean_ccm_exposure_investigations_acquisition_data %>%
+    left_join(
+      clean_ccm_investigations_data,
+      by = "investigation_number"
+    ) %>%
+    left_join(clean_ccm_exposures_data,
+      by = "exposure"
+    ) %>%
+    # including anything within an incubation period
+    filter(
+      investigation_record_type == "Case Investigation",
+      episode_date >= (lubridate::today() + lubridate::days(-14))
+    ) %>%
+    count(exposure,
+      location,
+      sort = TRUE
+    ) %>%
+    filter(n > 1)
+}
