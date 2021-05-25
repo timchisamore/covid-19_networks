af4e76da598e285f96258f34290a5da538273962
Tim Chisamore <tim.chisamore@gmail.com>
2021-05-25 16:24:18 -0400

    Removing unused packages.

--- requirements.R
+++ requirements.R
@@ -1,15 +1,8 @@
 library(conflicted)
 library(tidyverse)
 library(drake)
-library(gt)
-library(gtsummary)
 library(tidygraph)
 library(ggraph)
 conflict_prefer("filter", "dplyr")
 conflict_prefer("lag", "dplyr")
 dotenv::load_dot_env(here::here(".env"))
-set_gtsummary_theme(list(
-  "tbl_summary-fn:percent_fun" = function(x) {
-    scales::percent(x, accuracy = 1, suffix = NULL)
-  }
-))
